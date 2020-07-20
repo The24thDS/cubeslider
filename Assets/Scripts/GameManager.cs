6bb4a49b6898152d2f106610d73fffe697ce717e
The24thDS <davidsima@windowslive.com>
2020-07-20 21:08:18 +0300

    Initial version

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+  private bool hasGameEnded = false;
+  public float restartDelay = 2f;
+  public GameObject completeLevelUI;
+  public PlayerMovement movement;
+
+  public void EndGame()
+  {
+    if (!hasGameEnded)
+    {
+      hasGameEnded = true;
+      Debug.Log("GAME OVER");
+      Invoke("Restart", restartDelay);
+    }
+  }
+
+  void Restart()
+  {
+    SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+  }
+
+  public void LevelComplete()
+  {
+    movement.enabled = false;
+    completeLevelUI.SetActive(true);
+  }
+}
---
+++ Assets/Scripts/PlayerScore.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerScore : MonoBehaviour
+{
+  public Transform player;
+  public Text score;
+
+  // Update is called once per frame
+  void Update()
+  {
+    score.text = player.position.z.ToString("0");
+  }
+}
